ff99ea9a0d1515c51d0cb17923bae8e18e626fdd
Jules Ballaran <jules.ballaran@boom.camp>
2021-03-10 11:50:50 +0800

    test server side

--- pages/test/[id].js
+++ pages/test/[id].js
@@ -1,30 +1,34 @@
 import { useRouter } from "next/router"
 import SEO from "../../src/seo"
 
-const Test = ({ id }) => {
+const Test = ({ img }) => {
+  const { id } = useRouter()
   return (
     <div>
       <SEO
         description="test"
         title="test"
         siteTitle="test"
-        image={id === "1" ? "/assets/unnamed.jpg" : "/assets/void2.jpg"}
+        image={id === "1" ? img.t1 : img.t2}
       />
       <p>Test: {id}</p>
-      <img
-        src={id === "1" ? "/assets/unnamed.jpg" : "/assets/void2.jpg"}
-        alt="me"
-      />
+      <img src={id === "1" ? img.t1 : img.t2} alt="me" />
     </div>
   )
 }
 
 export async function getServerSideProps() {
   // Fetch data from external API
-  const { id } = await useRouter()
 
   // Pass data to the page via props
-  return { props: { id } }
+  return {
+    props: {
+      img: {
+        t1: "/assets/unnamed.jpg",
+        t2: "/assets/void2.jpg",
+      },
+    },
+  }
 }
 
 export default Test
